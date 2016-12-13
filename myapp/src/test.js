621ce0aacdab9a0b869985e37a5cf09f3de0b1a8
Lucas Monteiro <lucasmonteiro001@gmail.com>
2016-12-12 22:21:51 -0200

    teste

--- myapp/src/test.js
+++ myapp/src/test.js
@@ -7,5 +7,6 @@ describe('Array', function() {
         it('should return -1 when the value is not present', function() {
             assert.equal(-1, [1,2,3].indexOf(4));
         });
+        // test para ver se vai mais rapido
     });
 });
